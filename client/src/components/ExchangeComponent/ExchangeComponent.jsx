b647e981b39c39dca6b40db63d7f69495dd0ff9e
omowonuola <oyedmola@gmail.com>
2023-03-05 18:50:17 +0000

    updates the repo

--- client/src/components/ExchangeComponent/ExchangeComponent.jsx
+++ client/src/components/ExchangeComponent/ExchangeComponent.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Select, Space, Button, Input } from "antd";
 import "./ExchangeComponent.scss";
 
@@ -6,31 +6,53 @@ const handleChange = (value) => {
   console.log(`selected ${value}`);
 };
 
+const currencyOptions = [
+    { value: "USD", label: "USD" },
+    { value: "EUR", label: "EUR" },
+    { value: "GBP", label: "GBP" },
+];
+
+const cryptoOptions = [
+    { value: "BTC", label: "Bitcoin" },
+    { value: "ETH", label: "Ethereum" },
+    { value: "LTC", label: "Litecoin" },
+];
+
+
 const ExchangeComponent = () => {
+    const [currency, setCurrency] = useState("USD");
+    const [crypto, setCrypto] = useState("BTC");
+
+
+    const handleCurrencyChange = (value) => {
+        setCurrency(value);
+      };
+    
+    const handleCryptoChange = (value) => {
+    setCrypto(value);
+    };
+
+
   return (
     <div className="py-20 px-28 exchangeComponent">
       <h3>Exchange</h3>
       <Space className="mt-5" wrap>
         <div>
           <p>Currency from</p>
-          <Select
-            defaultValue="Bitcoin"
-            style={{
-              width: 120,
-            }}
-            onChange={handleChange}
-            options={[
-              {
-                value: "Ethereum",
-                label: "Ethereum",
-              },
-              {
-                value: "disabled",
-                label: "Disabled",
-                disabled: true,
-              },
-            ]}
-          />
+            <Select
+                style={{
+                    width: 120,
+                }}
+                defaultValue={currency}
+                onChange={handleCurrencyChange}
+            >
+                {currencyOptions.map((option) => (
+                    <Option value={option.value} key={option.value}>
+                    {option.label}
+                    </Option>
+                ))}
+            </Select>
+
         </div>
 
         <div>
@@ -42,21 +64,19 @@ const ExchangeComponent = () => {
 
         <div>
           <p>Currency to</p>
-
-          <Select
-            defaultValue="USD"
-            style={{
-              width: 120,
-            }}
-            onChange={handleChange}
-            options={[
-              {
-                value: "disabled",
-                label: "Disabled",
-                disabled: true,
-              },
-            ]}
-          />
+            <Select
+                style={{
+                    width: 120,
+                    }}
+                defaultValue={crypto}
+                onChange={handleCryptoChange}
+            >
+                {cryptoOptions.map((option) => (
+                <Option value={option.value} key={option.value}>
+                {option.label}
+                </Option>
+            ))}
+            </Select>
         </div>
         <div>
           <p>Amount</p>
@@ -71,3 +91,4 @@ const ExchangeComponent = () => {
 };
 
 export default ExchangeComponent;
+
